2dc45f64ee0a8bbfe731fd5d39b2e5a10cc0cb9d
Jay <jb.lambert23@gmail.com>
2021-03-06 22:38:25 -0500

    dump chess explorer trie results to output files

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,51 @@
 import * as parser from '@mliebelt/pgn-parser';
 import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
-fs.readFile('./test-png.txt', 'utf8', (err, data) => {
-  const moves = parser.parse(data, { startRule: 'games' });
-  console.log(moves.length);
+import { ChessExplorerTrie } from './chess-explorer-trie';
+
+const user = 'justuntinian';
+
+fs.readFile('./test-png.txt', 'utf8', (_, data) => {
+  const games = parser.parse(data, { startRule: 'games' });
+
+  const whiteGames = games.filter((game) => game.tags.White === user);
+  const whiteExplorer = createExplorer(whiteGames);
+  dumpExplorer(whiteExplorer, 'white');
+
+  const blackGames = games.filter((game) => game.tags.Black === user);
+  const blackExplorer = createExplorer(blackGames);
+  dumpExplorer(blackExplorer, 'black');
 });
+
+function createExplorer(games: parser.Game[]) {
+  const explorer = new ChessExplorerTrie();
+  for (const game of games) {
+    explorer.put(game);
+  }
+  return explorer;
+}
+
+function dumpExplorer(explorer: ChessExplorerTrie, path: string) {
+  const toJson = (value: any) => JSON.stringify(value, null, 1);
+  const toYaml = (value: any) => yaml.dump(value);
+
+  const _path = `out/${path}`;
+  fs.mkdirSync(_path, { recursive: true });
+
+  const rootJsonPath = `${_path}/root.json`;
+  fs.writeFileSync(rootJsonPath, toJson(explorer.root));
+  console.log('updated ' + rootJsonPath);
+
+  const notationJsonPath = `${_path}/notation.json`;
+  fs.writeFileSync(notationJsonPath, toJson(explorer.toNotation()));
+  console.log('updated ' + notationJsonPath);
+
+  const rootYamlPath = `${_path}/root.yaml`;
+  fs.writeFileSync(rootYamlPath, toYaml(explorer.root));
+  console.log('updated ' + rootYamlPath);
+
+  const notationYamlPath = `${_path}/notation.yaml`;
+  fs.writeFileSync(notationYamlPath, toYaml(explorer.toNotation()));
+  console.log('updated ' + notationYamlPath);
+}
