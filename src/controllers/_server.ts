2d8eeb2cdf5e0ff750df620e74af0680a5793a53
Jay <jb.lambert23@gmail.com>
2021-03-17 22:44:07 -0400

    add droptables flag

--- src/controllers/_server.ts
+++ src/controllers/_server.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-export async function connect() {
+export async function connect(dropTables = false) {
   await mongoose.connect('mongodb://localhost/chess-explorer', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -8,5 +8,11 @@ export async function connect() {
     useCreateIndex: true,
   });
   console.log('established connection');
+
+  if (dropTables) {
+    await mongoose.connection.collections['pgnlookups'].drop();
+    console.log('dropped pgnlookups');
+  }
+
   return;
 }
