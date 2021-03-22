7afcd76c23e5ca0e0cd39e65455c9cf0e881c49d
Jay <jb.lambert23@gmail.com>
2021-03-21 22:22:32 -0400

    add explorer service to get next moves

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import { connect } from './controllers/_server';
+import { getNextMoves } from './services/explorer.service';
 import { loadPlayerGames } from './services/loader.service';
 
 const player = 'justuntinian';
+const fen = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1';
+
+const reloadGames = true;
 
 async function main() {
   try {
-    await connect(true);
+    await connect(reloadGames);
 
-    console.log('loading games...');
-    await loadPlayerGames(player);
-    console.log('finished');
+    if (reloadGames) {
+      console.log('loading games...');
+      await loadPlayerGames(player);
+      console.log('finished');
+    }
+
+    console.log(`getting next moves for ${fen}`);
+    const nextMoves = await getNextMoves(fen);
+    console.log(nextMoves);
   } catch (e) {
     console.error(e);
   }
