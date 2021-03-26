0d2cf43d2ee31b3873507447cf20211ef66a5843
Jay <jb.lambert23@gmail.com>
2021-03-26 10:53:45 -0400

    implement express server

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,16 @@
+import * as express from 'express';
 import { connect } from './controllers/_server';
-import { getNextMoves } from './services/explorer.service';
-import { loadPlayerGames } from './services/loader.service';
 
-const player = 'justuntinian';
-const fen = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1';
+const url = 'localhost';
+const port = 3000;
+const app = express();
 
-const reloadGames = true;
-
-async function main() {
+app.listen(port, async () => {
+  console.log(`application listening on ${url}:${port}`);
   try {
-    await connect(reloadGames);
-
-    if (reloadGames) {
-      console.log('loading games...');
-      await loadPlayerGames(player);
-      console.log('finished');
-    }
-
-    console.log(`getting next moves for ${fen}`);
-    const nextMoves = await getNextMoves(fen);
-    console.log(nextMoves);
-  } catch (e) {
-    console.error(e);
+    await connect();
+  } catch (ex) {
+    console.log('failed to connect to database!');
+    console.log(ex);
   }
-}
-
-(async () => main())();
+});
