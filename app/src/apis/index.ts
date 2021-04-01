64bdd17d72816545244447a98354d397f8bb98f0
Jay <jb.lambert23@gmail.com>
2021-04-01 17:04:28 -0400

    create fetch hook with body

--- app/src/apis/index.ts
+++ app/src/apis/index.ts
@@ -10,5 +10,14 @@ export const useFetch = <T = any, U = any>(
   options?: SWRConfiguration
 ) => useSWR<T, U>(url, fetcher, options);
 
+export const useFetchBody = <T = any, U = any>(
+  url: string,
+  data?: any,
+  options?: SWRConfiguration
+) => useSWR<T, U>(url, () => poster(url, data), options);
+
 export const fetcher = <T>(url: string) =>
   axios.get<T>(buildUrl(url)).then((res) => res.data);
+
+export const poster = <T>(url: string, data?: any) =>
+  axios.post<T>(buildUrl(url), data).then((res) => res.data);
