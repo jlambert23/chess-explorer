fe30be357ebc000c579c3d2bafdf25baca728bbe
Jay <jb.lambert23@gmail.com>
2021-04-29 13:47:30 -0400

    add api env variable

--- app/src/apis/index.ts
+++ app/src/apis/index.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import useSWR, { SWRConfiguration } from 'swr';
 
-export const baseUrl = 'http://localhost:3001/';
-
-export const buildUrl = (url: string) => `${baseUrl}${url}`;
+axios.defaults.baseURL = process.env.REACT_APP_API;
 
 export const useFetch = <T = any, U = any>(
   url: string,
@@ -17,7 +15,7 @@ export const useFetchBody = <T = any, U = any>(
 ) => useSWR<T, U>(url, () => poster(url, data), options);
 
 export const fetcher = <T>(url: string) =>
-  axios.get<T>(buildUrl(url)).then((res) => res.data);
+  axios.get<T>(url).then((res) => res.data);
 
 export const poster = <T>(url: string, data?: any) =>
-  axios.post<T>(buildUrl(url), data).then((res) => res.data);
+  axios.post<T>(url, data).then((res) => res.data);
