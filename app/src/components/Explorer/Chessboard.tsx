ca7429e7ed969fa54f371f0335eb350340b2a28c
Jay <jb.lambert23@gmail.com>
2021-05-03 13:52:46 -0400

    add flip color button

--- app/src/components/Explorer/Chessboard.tsx
+++ app/src/components/Explorer/Chessboard.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import Chessground, { Color, DrawShape, Key } from 'react-chessground';
 import 'react-chessground/dist/styles/chessground.css';
 
+import { ReactComponent as FlipSvg } from '../../images/flip.svg';
 import { Move } from '../../models/explorer.model';
 
 interface ChessboardProps {
@@ -9,30 +11,52 @@ interface ChessboardProps {
   lastMove?: Key[];
 }
 
+const FlipButton = ({ onClick }: { onClick?: () => void }) => (
+  <button onClick={onClick} className='focus:outline-none'>
+    <div className='text-0 w-6'>
+      <FlipSvg />
+    </div>
+  </button>
+);
+
 const Chessboard = ({
   position = 'start',
   hover,
   lastMove = [],
-}: ChessboardProps) => (
-  <div className='w-screen-80 h-screen-80'>
-    <Chessground
-      width='100%'
-      height='100%'
-      fen={position}
-      selected={''}
-      lastMove={lastMove}
-      drawable={{
-        autoShapes: hover?.move
-          ? [
-              {
-                brush: 'green',
-                dest: hover.move.to,
-                orig: hover.move.from,
-              } as DrawShape,
-            ]
-          : [],
-      }}
-    />
-  </div>
-);
+}: ChessboardProps) => {
+  const [orientation, setOrientation] = useState('white' as Color);
+
+  const flip = () => {
+    setOrientation(orientation === 'white' ? 'black' : 'white');
+  };
+
+  return (
+    <div className='bg-0 rounded p-7 w-screen-80 h-screen-80'>
+      <div className='float-right'>
+        <div className='fixed z-10 -m-6'>
+          <FlipButton onClick={flip} />
+        </div>
+      </div>
+      <Chessground
+        width='100%'
+        height='100%'
+        fen={position}
+        selected={''}
+        lastMove={lastMove}
+        orientation={orientation}
+        drawable={{
+          autoShapes: hover?.move
+            ? [
+                {
+                  brush: 'green',
+                  dest: hover.move.to,
+                  orig: hover.move.from,
+                } as DrawShape,
+              ]
+            : [],
+        }}
+      />
+    </div>
+  );
+};
 export default Chessboard;
